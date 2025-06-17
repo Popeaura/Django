6e94d3e46e8f2a232b9f04c8219ff9560a24b0aa
popeaura <popeahura@gmail.com>
2025-06-17 15:24:21 +0300

    I uploaded my modifed project's folder

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem  # ← FIXED: changed ',' to '.'
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})  # ← FIXED: "todos.html" as a string
