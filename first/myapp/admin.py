95d8dbad7d880616c7f4041ff7ff60b579440b99
popeaura <popeahura@gmail.com>
2025-06-17 10:18:51 +0300

    I uploaded my modifed project's folder

--- first/myapp/admin.py
+++ first/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ToolItem
+from .models import TodoItem
 
 # Register your models here.
-admin.site.register(ToolItem)
+admin.site.register(TodoItem)
