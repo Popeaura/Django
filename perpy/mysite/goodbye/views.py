6107227f5e133ec34b9ffbf6657e062b3b56e021
popeaura <popeahura@gmail.com>
2025-07-09 19:09:18 +0300

    I added my modifed folder for my project

--- perpy/mysite/goodbye/views.py
+++ perpy/mysite/goodbye/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('welcome to your first django test !')
+def goodbye(request):
+    return HttpResponse('It was nice meeting you tchuss !')
