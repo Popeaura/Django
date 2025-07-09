2b44a9447b581b54a93f2dbf00175d3cc3134c89
popeaura <popeahura@gmail.com>
2025-07-09 16:12:17 +0300

    I added my modifed folder for my project

--- perpy/mysite/hello/models.py
+++ perpy/mysite/hello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.CharField(max_length=50)
+    text = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}: {self.text[:20]}"
--- perpy/mysite/hello/views.py
+++ perpy/mysite/hello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse (" Good Morning AFRICA")
+    messages = Message.objects.all().order_by('-sent_at')
+    context = {'name': 'Ahura pope', 'messages': messages}
+    return render(request,'hello/greeting.html', context)
+
+
