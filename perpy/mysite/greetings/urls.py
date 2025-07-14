8c37340f993ab01b9507adbcb13a42f69963581e
popeaura <popeahura@gmail.com>
2025-07-14 17:54:45 +0300

    I added my modifed folder for my project

--- perpy/mysite/greetings/urls.py
+++ perpy/mysite/greetings/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path ('',views.welcome , name='welcome'),
-    path ('',views.goodbye , name ='goodbye')
 ]
--- perpy/mysite/mysite/settings.py
+++ perpy/mysite/mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello',
     'greetings',
-    
+    'goodbye',
+
 ]
 
 MIDDLEWARE = [
