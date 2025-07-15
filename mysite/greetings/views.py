4b733ac255fef71929b5e29f1f9b372585eac0e0
popeaura <popeahura@gmail.com>
2025-07-15 16:54:48 +0300

    I added my modifed folder

--- mysite/greetings/views.py
+++ mysite/greetings/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     context = {
-        'name':'Kyler Gray',
+        'name':'Gray',
         'hobbies':['coding','video games','Music', 'Travelling' ]
     }
     return render (request, 'greetings/welcome.html',context)
+
