b8ff5b2a60e09a7b95e9a6db94a74eca5982470d
popeaura <popeahura@gmail.com>
2025-07-16 20:21:56 +0300

    I added my modifed folder

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('greetings/', include('greetings.urls')),
+    path('greetings/,', include('messages.html')),
 
 ]
