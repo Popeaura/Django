98a08315545faaae713f8a9af425b8fd1360386f
popeaura <popeahura@gmail.com>
2025-07-16 20:16:15 +0300

    I added my modifed folder

--- mysite/greetings/urls.py
+++ mysite/greetings/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('',view.show_messages, name='show_messages' ),
+    path('',views.show_messages, name='show_messages' ),
 ]
