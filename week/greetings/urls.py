66d458181389bd5335f69478c91b06d5730a44a9
popeaura <popeahura@gmail.com>
2025-07-20 19:43:03 +0300

    I added my modifed folder

--- week/greetings/urls.py
+++ week/greetings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='welome')
+    path('', views.welcome, name='welcome'),
+    path('messages/', views.show_messages, name='show_messages'),
 ]
