1047eb6624c72508c5436b115b3b690f65dbf307
popeaura <popeahura@gmail.com>
2025-07-29 19:59:09 +0300

    I added my modifed folder

--- reborn/reborn/urls.py
+++ reborn/reborn/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('greetings/', include('welcome.urls')),
+    path('welcome/', include('welcome.urls')),
 ]
