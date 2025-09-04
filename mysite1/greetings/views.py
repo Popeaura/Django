0d1163719261d15482bdbd0ec2ebdd22f2df5f22
Popeaura <popeahura@gmail.com>
2025-09-04 16:38:38 +0300

    I added my modified projects folder

--- mysite1/greetings/views.py
+++ mysite1/greetings/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Doctor, Patient
+
 def patients_list(request):
     patients = Patient.objects.all()
-    if patients.exists():
-        return HttpResponse(", ".join([patient.name for patient in patients]))
-    else:
-        return HttpResponse("No patients found.")
+    return render(request, "greetings/patients_list.html", {"patients": patients})
 
 def doctors_list(request):
     doctors = Doctor.objects.all()
-    if doctors.exists():
-        return HttpResponse(", ".join([doctor.name for doctor in doctors]))
-    else:
-        return HttpResponse("No doctors found.")
+    return render(request, "greetings/doctors_list.html", {"doctors": doctors})
